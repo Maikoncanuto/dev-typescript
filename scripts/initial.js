4eeaebb5ccac2790e8cb438d790c30e890c6a878
Maikon Canuto <maikon.pedrosa@altran-ais.com.br>
2017-08-25 11:35:54 -0300

    Add funcoes, variaveis e cast

--- scripts/initial.js
+++ scripts/initial.js
@@ -4,7 +4,32 @@ var texto = "Aqui é um texto";
 var texto1 = 'Arqui é outro texto';
 var bool = true;
 var template = "" + texto;
+var naoSeiTipo = "10";
+//Arrays/list/enum
+var arry = [1, 2, 3];
+var genericArray = [2, 2,];
+var tupla = ["TS", 1];
+var Dia;
+(function (Dia) {
+    Dia[Dia["SEGUNDA"] = 0] = "SEGUNDA";
+    Dia[Dia["TERCA"] = 2] = "TERCA";
+})(Dia || (Dia = {}));
+;
+var dia = Dia.SEGUNDA;
+console.log("ENUM: ", dia);
+//cast
+var stringTamanho = naoSeiTipo.length;
+console.log("CAST 1: ", stringTamanho);
+var outraString = naoSeiTipo.length;
+console.log("CAST 2: ", outraString);
 //Funções
+//Funcao com parametro opcional 
+function func0(param1, param2) {
+    if (param2 !== undefined) {
+        return param1 + param2;
+    }
+    return param1;
+}
 function func1() {
     return 1;
 }
--- scripts/initial.ts
+++ scripts/initial.ts
@@ -4,8 +4,34 @@ let texto: string = "Aqui é um texto";
 let texto1: string = 'Arqui é outro texto';
 let bool: boolean = true;
 let template: string =  `${texto}`;
+let naoSeiTipo: any = "10";   
+
+//Arrays/list/enum
+let arry: number[] = [1, 2, 3];
+let genericArray: Array<number> = [2, 2,];
+let tupla:  [string, number] = ["TS", 1];
+
+enum Dia{SEGUNDA, TERCA = 2};
+let dia: Dia = Dia.SEGUNDA;
+console.log("ENUM: ", dia);
+
+//cast
+let stringTamanho = (<string>naoSeiTipo).length;
+console.log("CAST 1: ", stringTamanho);
+let outraString = (naoSeiTipo as string).length;
+console.log("CAST 2: ", outraString);
 
 //Funções
+
+//Funcao com parametro opcional 
+function func0(param1: number, param2?: number): number{
+    if(param2 !== undefined){
+        return param1 + param2;
+    }
+    
+    return param1;
+}
+
 function func1(): number{
     return 1;
 }
