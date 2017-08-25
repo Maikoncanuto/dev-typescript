75161ad6577ed08f4c1442ce2dbd81092387adc3
Maikon Canuto <maikon.pedrosa@altran-ais.com.br>
2017-08-25 10:45:46 -0300

    Variaveis, funcoes e condicionais

--- scripts/initial.js
+++ scripts/initial.js
@@ -1,2 +1,50 @@
+//Variaveis 
 var numero = 10;
-console.log("NUMERO: ", numero);
+var texto = "Aqui é um texto";
+var texto1 = 'Arqui é outro texto';
+var bool = true;
+var template = "" + texto;
+//Funções
+function func1() {
+    return 1;
+}
+function func1P(param) {
+    return param;
+}
+function func2() {
+    return "Retorno";
+}
+function func2P(param) {
+    return param;
+}
+function func3() {
+    return 'outro retorno';
+}
+function func3P(param) {
+    return param;
+}
+function func4() {
+    return false;
+}
+function func4P(param) {
+    return param;
+}
+function func5() {
+    return;
+}
+function func5P(param) {
+    return;
+}
+function func6() {
+    return;
+}
+//Condicionais 
+if (func1P(10) === numero) {
+    console.log('CONDICIONAL 1: ', func1P(10) === numero);
+}
+if (func1P(5) === numero) {
+    console.log('CONDICIONAL 2: ', func1P(5) === numero);
+}
+if (func3P(template) === texto) {
+    console.log('CONDICIONAL 2: ', func3P(template) === texto);
+}
--- scripts/initial.ts
+++ scripts/initial.ts
@@ -1,3 +1,64 @@
+//Variaveis 
 let numero: number = 10;
+let texto: string = "Aqui é um texto";
+let texto1: string = 'Arqui é outro texto';
+let bool: boolean = true;
+let template: string =  `${texto}`;
 
-console.log("NUMERO: ", numero);
+//Funções
+function func1(): number{
+    return 1;
+}
+
+function func1P(param: number): number{
+    return param;
+}
+
+function func2(): string{
+    return "Retorno";
+}
+
+function func2P(param: string): string{
+    return param;
+}
+
+function func3(): string{
+    return 'outro retorno';
+}
+
+function func3P(param: string): string{
+    return param;
+}
+
+function func4(): boolean{
+    return false;
+}
+
+function func4P(param: boolean): boolean{
+    return param;
+}
+
+function func5(): void{
+    return;
+}
+
+function func5P(param: void): void{
+    return;
+}
+
+function func6(){
+    return;
+}
+
+//Condicionais 
+if(func1P(10) === numero){
+    console.log('CONDICIONAL 1: ', func1P(10) === numero);
+}
+
+if(func1P(5) === numero){
+    console.log('CONDICIONAL 2: ', func1P(5) === numero);
+}
+
+if(func3P(template) === texto){
+    console.log('CONDICIONAL 2: ', func3P(template) === texto);
+}
